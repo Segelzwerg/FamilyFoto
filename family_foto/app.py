6a7926ff5c93a9d27bf5e2877f6ae472dbb04a2b
Segelzwerg <marcel.haas@hhu.de>
2020-08-09 22:25:28 +0200

    FIX: use data not field

--- family_foto/app.py
+++ family_foto/app.py
@@ -40,7 +40,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password):
+        if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
