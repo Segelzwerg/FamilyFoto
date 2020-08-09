9e560b7332df3f909f872f53ce1aa760359e114c
Segelzwerg <marcel.haas@hhu.de>
2020-08-09 21:44:39 +0200

    ADD: test for login on user

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,12 +1,22 @@
-import unittest
-
 from flask_api import status
+from flask_login import current_user
 
 from family_foto.app import app
+from tests.base_test_case import BaseTestCase
 
 
-class LoginTestCase(unittest.TestCase):
+class LoginTestCase(BaseTestCase):
     def test_login_page_is_available(self):
         with app.test_client() as client:
             response = client.get('/login')
             self.assertTrue(status.is_success(response.status_code))
+
+    def test_login(self):
+        with self.client:
+            response = self.client.post('/login',
+                                        data={'username': 'marcel',
+                                              'password': '1234'},
+                                        follow_redirects=True)
+            self.assertTrue(status.is_success(response.status_code))
+            self.assertEqual(current_user.username, 'marcel')
+            self.assertFalse(current_user.is_anonymous())
