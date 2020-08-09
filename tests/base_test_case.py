85d9617df99761fc7a66a9723fb7ae826dce4e01
Segelzwerg <marcel.haas@hhu.de>
2020-08-09 23:29:03 +0200

    REFACTOR: move user creation logic back as it is needed in other test cases

--- tests/base_test_case.py
+++ tests/base_test_case.py
@@ -2,21 +2,27 @@ from flask_testing import TestCase
 
 from family_foto.app import app
 from family_foto.models import db
+from family_foto.models.user import User
 
 
+# pylint: disable=invalid-name
 class BaseTestCase(TestCase):
     """
     Basic Test Case that setups the flask app with a db.
     """
+
     def create_app(self):
         app.config.from_object('family_foto.config.TestConfiguration')
-        # pylint: disable=attribute-defined-outside-init, invalid-name
-        self.db = db
+
         return app
 
     def setUp(self):
-        self.db.create_all()
+        db.create_all()
+        user = User(username='marcel')
+        user.set_password('1234')
+        db.session.add(user)
+        db.session.commit()
 
     def tearDown(self):
-        self.db.session.remove()
-        self.db.drop_all()
+        db.session.remove()
+        db.drop_all()
