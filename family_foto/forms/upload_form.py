4cd684e49ee89f07736c2eb24034bba731cdbad1
Segelzwerg <marcel.haas@hhu.de>
2020-08-10 00:58:35 +0200

    ADD: upload form

---
+++ family_foto/forms/upload_form.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileRequired, FileAllowed
+from wtforms import SubmitField, MultipleFileField
+
+
+class UploadForm(FlaskForm):
+    """
+    Data form of upload mask.
+    """
+    file = MultipleFileField('File',
+                             validators=[FileRequired(),
+                                         FileAllowed(['jpg', 'png'],
+                                                     'Images Only!')])
+    submit = SubmitField('Upload')
