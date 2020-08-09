7caf7dacd21bf897f76d8b29b28e743c274d74b3
Segelzwerg <marcel.haas@hhu.de>
2020-08-10 00:58:24 +0200

    ADD: photo entity

---
+++ family_foto/models/photo.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from family_foto.models import db
+
+
+class Photo(db.Model):
+    # pylint: disable=too-few-public-methods
+    """
+    Database entity of a photo.
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(64), index=True, unique=True)
+    upload_date = db.Column(db.DateTime, default=datetime.utcnow)
