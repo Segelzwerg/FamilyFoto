54009c1753416eb51a9830c7f07d9c3f42103560
Segelzwerg <marcel.haas@hhu.de>
2020-08-09 15:26:04 +0200

    UPDATE: use index instead of hello world

--- family_foto/app.py
+++ family_foto/app.py
@@ -18,8 +18,8 @@ login_manager.init_app(app)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
