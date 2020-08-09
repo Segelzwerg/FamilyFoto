f2a92bf883a654448fb76d5fc9f4b80bd18cf112
Segelzwerg <marcel.haas@hhu.de>
2020-08-09 22:28:41 +0200

    REFACTOR: add database field

--- tests/base_test_case.py
+++ tests/base_test_case.py
@@ -1,4 +1,5 @@
 from flask_testing import TestCase
+
 from family_foto.app import app
 from family_foto.models import db
 
@@ -6,11 +7,12 @@ from family_foto.models import db
 class BaseTestCase(TestCase):
     def create_app(self):
         app.config.from_object('family_foto.config.TestConfiguration')
+        self.db = db
         return app
 
     def setUp(self):
-        db.create_all()
+        self.db.create_all()
 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+        self.db.session.remove()
+        self.db.drop_all()
