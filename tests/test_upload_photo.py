24522c35454681bf46907bc3880d91a127d8d5bf
Segelzwerg <marcel.haas@hhu.de>
2020-08-10 01:47:12 +0200

    ADD: test for wrong file type FIX: check for actual upload

--- tests/test_upload_photo.py
+++ tests/test_upload_photo.py
@@ -1,9 +1,13 @@
+import os
 from io import BytesIO
+from shutil import rmtree
 from unittest.mock import patch
 
 from flask_api import status
 
 from tests.base_test_case import BaseTestCase
+
+PHOTOS_SAVE_PATH = './photos'
 
 
 class PhotoUploadTestCase(BaseTestCase):
@@ -12,12 +16,12 @@ class PhotoUploadTestCase(BaseTestCase):
     """
 
     def setUp(self):
-        pass
         self.patcher = patch('flask_login.utils._get_user')
         self.mock_current_user = self.patcher.start()
 
     def tearDown(self):
-        pass
+        if os.path.exists(PHOTOS_SAVE_PATH):
+            rmtree(PHOTOS_SAVE_PATH)
         self.patcher.stop()
 
     def test_upload(self):
@@ -31,3 +35,18 @@ class PhotoUploadTestCase(BaseTestCase):
                                     content_type='multipart/form-data',
                                     data=file)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertIn('foto.jpg', os.listdir(PHOTOS_SAVE_PATH))
+
+    def test_upload_wrong_file_type(self):
+        """
+        Tests if a doc upload fails.
+        """
+        file = dict(
+            file=(BytesIO(b'my file contents'), "foto.doc"),
+        )
+        response = self.client.post('/upload',
+                                    content_type='multipart/form-data',
+                                    data=file)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        if os.path.exists(PHOTOS_SAVE_PATH):
+            self.assertNotIn('foto.jpg', os.listdir(PHOTOS_SAVE_PATH))
