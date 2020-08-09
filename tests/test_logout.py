b999607e6ea03a30a744bb8a8f298cfa8bd18440
Segelzwerg <marcel.haas@hhu.de>
2020-08-09 23:41:43 +0200

    UPDATE: assert for redirect

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -1,3 +1,4 @@
+from flask_api import status
 from flask_login import current_user
 
 from tests.base_test_case import BaseTestCase
@@ -14,12 +15,13 @@ class LogoutTestCase(BaseTestCase):
         """
 
         with self.client:
-            response = self.client.post('/login',
-                                        data={'username': 'marcel',
-                                              'password': '1234'},
-                                        follow_redirects=True)
-            self.client.get('/logout')
+            self.client.post('/login',
+                             data={'username': 'marcel',
+                                   'password': '1234'},
+                             follow_redirects=True)
+            response = self.client.get('/logout')
 
             username = current_user.username if not current_user.is_anonymous else 'anonymous'
             self.assertTrue(current_user.is_anonymous, msg=f'user <{username}> is '
                                                            f'still logged in')
+            self.assertTrue(status.is_redirect(response.status_code))
