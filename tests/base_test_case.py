ae1f8cd0787e422a96157d97dde590b4630758c3
Segelzwerg <marcel.haas@hhu.de>
2020-08-10 03:27:20 +0200

    FIX: db behaviour

--- tests/base_test_case.py
+++ tests/base_test_case.py
@@ -1,6 +1,7 @@
 from flask_testing import TestCase
 
 from family_foto.app import app
+from family_foto.config import TestConfiguration
 from family_foto.models import db
 from family_foto.models.user import User
 
@@ -12,16 +13,17 @@ class BaseTestCase(TestCase):
     """
 
     def create_app(self):
-        app.config.from_object('family_foto.config.TestConfiguration')
-
+        app.config['WTF_CSRF_ENABLED'] = False
         return app
 
     def setUp(self):
         db.create_all()
-        user = User(username='marcel')
-        user.set_password('1234')
-        db.session.add(user)
-        db.session.commit()
+        exists = User.query.filter_by(username='marcel').first()
+        if not exists:
+            user = User(username='marcel')
+            user.set_password('1234')
+            db.session.add(user)
+            db.session.commit()
 
     def tearDown(self):
         db.session.remove()
