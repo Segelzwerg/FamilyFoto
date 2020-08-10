2af4fcb07d34c83fe2f3257de596102daf3969b7
Segelzwerg <marcel.haas@hhu.de>
2020-08-10 03:30:29 +0200

    ADD: relation between user and photo

--- family_foto/models/photo.py
+++ family_foto/models/photo.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from sqlalchemy import ForeignKey
+
 from family_foto.models import db
 
 
@@ -11,3 +13,4 @@ class Photo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(64), index=True, unique=True)
     upload_date = db.Column(db.DateTime, default=datetime.utcnow)
+    user = db.Column(db.Integer, ForeignKey('user.id'))
