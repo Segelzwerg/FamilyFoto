2e2a6d68237208e4cc1c4a2761e809c2bab8457d
Segelzwerg <marcel.haas@hhu.de>
2020-08-12 18:42:26 +0200

    ADD: url column

--- family_foto/models/photo.py
+++ family_foto/models/photo.py
@@ -12,5 +12,6 @@ class Photo(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(64), index=True, unique=True)
+    url = db.Column(db.String(128), unique=True)
     upload_date = db.Column(db.DateTime, default=datetime.utcnow)
     user = db.Column(db.Integer, ForeignKey('user.id'))
