dcaf3b134cb3e1b47403fa6bf6f24178fd7bd940
Segelzwerg <marcel.haas@hhu.de>
2020-08-23 16:04:09 +0200

    ADD: test for original photo getter

--- tests/test_gallery.py
+++ tests/test_gallery.py
@@ -3,6 +3,7 @@ from io import BytesIO
 from shutil import rmtree
 from unittest.mock import patch
 
+from flask_api import status
 from flask_login import current_user
 
 from family_foto.models import db
@@ -61,3 +62,13 @@ class GalleryTestCase(BaseTestCase):
         all_photos = Photo.query.all()
         self.assertIn(other_photo, all_photos)
         self.assertNotIn(other_photo, photos)
+
+    def test_get_original_photo(self):
+        file = dict(
+            file=(BytesIO(b'my file contents'), "foto.jpg"),
+        )
+        self.client.post('/upload',
+                         content_type='multipart/form-data',
+                         data=file)
+        response = self.client.get('/photo/foto.jpg')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
