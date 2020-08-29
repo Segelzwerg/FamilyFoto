6afac705845769f0cafc6b321fcf841a43f378f1
Segelzwerg <marcel.haas@hhu.de>
2020-08-29 15:18:00 +0200

    ADD: test for multiple user sharing

--- tests/test_user_settings.py
+++ tests/test_user_settings.py
@@ -1,10 +1,5 @@
-from unittest.mock import patch
-
-from flask_login import current_user
-
 from family_foto.app import add_user
 from family_foto.models import db
-from family_foto.models.user import User
 from family_foto.models.user_settings import UserSettings
 from tests.base_test_case import BaseTestCase
 
@@ -16,21 +11,37 @@ class UserSettingsTestCase(BaseTestCase):
 
     def setUp(self):
         super().setUp()
-        self.patcher = patch('flask_login.utils._get_user')
-        self.mock_current_user = self.patcher.start()
-        self.mock_current_user.return_value = User(id=1,
-                                                   username='marcel')
+        self.user = add_user('settings_user', 'settings')
+
+    def tearDown(self):
+        super().tearDown()
 
     def test_relation(self):
-        user = add_user('new_user', '1234')
-        self.assertIsNotNone(user.settings)
-        self.assertEqual(user.id, user.settings.user_id)
+        """
+        Tests if the id are set correctly.
+        """
+        self.assertIsNotNone(self.user.settings)
+        self.assertEqual(self.user.id, self.user.settings.user_id)
 
     def test_add_user_to_sharing(self):
-        other_user = User.query.filter_by(username='lea').first()
-        if not other_user:
-            other_user = add_user('lea', '1234')
-        current_user.share_all_with(other_user)
+        """
+        Tests if the share with does work correctly.
+        """
+        other_user = add_user('lea', '1234')
+        self.user.share_all_with(other_user)
         db.session.commit()
-        shared_with = UserSettings.query.get(current_user.id)
-        self.assertListEqual([other_user], shared_with.share_all)
+        settings = UserSettings.query.get(self.user.id)
+        self.assertListEqual([other_user], settings.share_all)
+
+    def test_add_multiple_users_to_sharing(self):
+        """
+        Tests sharing photos with multiple users.
+        """
+        first_user = add_user('first', '5678')
+        second_user = add_user('second', '9876')
+        user_list = [first_user, second_user]
+
+        self.user.share_all_with(user_list)
+        settings = UserSettings.query.get(self.user.id)
+
+        self.assertListEqual(user_list, settings.share_all)
