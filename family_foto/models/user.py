11429c52d0a52f894cde21ad4ea68737d079629e
Segelzwerg <marcel.haas@hhu.de>
2020-08-29 12:44:50 +0200

    ADD: user settings to user

--- family_foto/models/user.py
+++ family_foto/models/user.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import relationship
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from family_foto.models import db
+from family_foto.models.user_settings import UserSettings
 
 
 class User(UserMixin, db.Model):
@@ -12,6 +13,8 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    settings = relationship('UserSettings', foreign_keys='UserSettings.user_id',
+                            back_populates='user', uselist=False)
     photos = relationship('Photo')
 
     def __repr__(self):
@@ -37,3 +40,9 @@ class User(UserMixin, db.Model):
         :return: List of photo objects.
         """
         return User.query.filter_by(id=self.id).first().photos
+
+    def share_all_with(self, other_user):
+        settings: UserSettings = UserSettings.query.get(self.id)
+        if not settings:
+            raise AttributeError(f'There are no user settings for the user with the id: {self.id}')
+        settings.share_all_photos_with(other_user)
