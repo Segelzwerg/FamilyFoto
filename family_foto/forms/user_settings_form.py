3ebb7509c91148010023988248b243e838e786b5
Segelzwerg <marcel.haas@hhu.de>
2020-09-01 13:08:55 +0200

    UPDATE: use custom multi select

--- family_foto/forms/user_settings_form.py
+++ family_foto/forms/user_settings_form.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectMultipleField, SubmitField
+from wtforms import SubmitField
+
+from family_foto.forms.fields.multi_checkbox_field import MultiCheckboxField
 
 
 class UserSettingsForm(FlaskForm):
     """
     Form of user settings.
     """
-    share_with = SelectMultipleField('ShareWith', choices=[])
+    share_with = MultiCheckboxField('ShareWith', choices=[])
     submit = SubmitField('Save changes')
