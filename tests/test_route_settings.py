7a00d9c56d502d984be199a6da74f5a191af7a97
Segelzwerg <marcel.haas@hhu.de>
2020-09-01 12:35:29 +0200

    ADD: tests for settings route

---
+++ tests/test_route_settings.py
@@ -0,0 +1,31 @@
+from unittest.mock import patch
+
+from flask_api import status
+
+from family_foto.models.user import User
+from tests.base_test_case import BaseTestCase
+
+
+class RouteSettingsTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.patcher = patch('flask_login.utils._get_user')
+        self.mock_current_user = self.patcher.start()
+        self.mock_current_user.return_value = User(id=1,
+                                                   username='marcel')
+
+    def tearDown(self):
+        self.patcher.stop()
+        super().tearDown()
+
+    def test_settings_route(self):
+        """
+        Tests the user settings route.
+        """
+        response = self.client.get('/settings')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+    def test_require_login_settings(self):
+        self.patcher.stop()
+        response = self.client.get('/settings')
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
