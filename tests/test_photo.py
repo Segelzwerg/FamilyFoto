27a8ee269a15dd7b2d55e1fe8515d91ea1dc1b04
Segelzwerg <marcel.haas@hhu.de>
2020-09-03 15:17:16 +0200

    ADD: test for meta data of image

--- tests/test_photo.py
+++ tests/test_photo.py
@@ -1,11 +1,31 @@
+import os
+from shutil import copyfile, rmtree
+
+import pytest
+from werkzeug.datastructures import FileStorage
+
 from family_foto.models.photo import Photo
 from tests.base_test_case import BaseTestCase
+
+PHOTOS_SAVE_PATH = './photos'
 
 
 class PhotoTestCase(BaseTestCase):
     """
     Tests the functionality of the Photo Entity.
     """
+
+    @pytest.fixture
+    def example_image(self):
+        file_path = 'data/example-image.jpg'
+        file = open(file_path, 'rb')
+        return FileStorage(stream=file, filename=file_path, content_type='image')
+
+    def tearDown(self):
+        if os.path.exists(PHOTOS_SAVE_PATH):
+            rmtree(PHOTOS_SAVE_PATH)
+        super().tearDown()
+
     def test_path(self):
         """
         Tests the path property.
@@ -13,3 +33,39 @@ class PhotoTestCase(BaseTestCase):
         filename = 'test.jpg'
         photo = Photo(filename=filename, url='/photos/test.jpg')
         self.assertEqual(f'./photos/{filename}', photo.path)
+
+    def test_meta(self):
+        """
+        Test the meta data property.
+        """
+        if not os.path.exists('./photos'):
+            os.mkdir(PHOTOS_SAVE_PATH)
+        copyfile('./data/example.jpg', f'{PHOTOS_SAVE_PATH}/example.jpg')
+        photo = Photo(filename='example.jpg', url='/photos/example.jpg')
+
+        expected_dict = dict(DateTime='2020:08:18 12:44:08',
+                             ExifImageWidth='4208',
+                             ExifImageHeight='3120',
+                             Flash='0',
+                             FNumber='2.2',
+                             FocalLength='3.5',
+                             ISOSpeedRatings='113',
+                             Make='BullittGroupLimited',
+                             Model='S41')
+        test_status, test_message = self._test_meta(expected_dict, photo.meta)
+        self.assertTrue(test_status, msg=test_message)
+
+    @staticmethod
+    def _test_meta(expected_dict, meta):
+        keys_not_in = {k: v for k, v in expected_dict.items() if k not in meta.keys()}
+        msg = f'photo.meta does not contain following keys:{keys_not_in}' if len(
+            keys_not_in) > 0 else ''
+        different_values = {}
+        for key, val1 in expected_dict.items():
+            val2 = meta.get(key)
+            if val2 is not None and val1 != val2:
+                different_values[key] = f'{val1, val2}'
+        msg = msg + f'\nfollowing values are different {different_values}'
+        if len(different_values) == 0 and len(keys_not_in) == 0:
+            return True, msg
+        return False, msg
