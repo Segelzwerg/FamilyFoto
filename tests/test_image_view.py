ab4b788f0e5158e83e44baa5983c1db87776ee2c
Segelzwerg <marcel.haas@hhu.de>
2020-09-04 15:05:34 +0200

    ADD: doc string

--- tests/test_image_view.py
+++ tests/test_image_view.py
@@ -9,6 +9,15 @@ class ImageViewTestCase(BaseLoginTestCase, BasePhotoTestCase):
     """
 
     def test_route(self):
+        """
+        Tests if the route works for an example image.
+        """
         db.session.add(self.photo)
         db.session.commit()
         self.client.get('/image/example.jpg')
+
+    def test_only_show_allowed_photos(self):
+        """
+        Tests if the user can only see photos he/she/it has permission to view.
+        """
+        pass
