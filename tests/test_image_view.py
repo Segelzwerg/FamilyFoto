e3bc88c328e191db308ba9e9916b605d41b8cc9c
Segelzwerg <marcel.haas@hhu.de>
2020-09-04 15:02:51 +0200

    FIX: add photo to db before using it

--- tests/test_image_view.py
+++ tests/test_image_view.py
@@ -1,10 +1,14 @@
+from family_foto.models import db
+from tests.BasePhotoTestCase import BasePhotoTestCase
 from tests.base_login_test_case import BaseLoginTestCase
 
 
-class ImageViewTestCase(BaseLoginTestCase):
+class ImageViewTestCase(BaseLoginTestCase, BasePhotoTestCase):
     """
     Tests the image route.
     """
 
     def test_route(self):
+        db.session.add(self.photo)
+        db.session.commit()
         self.client.get('/image/example.jpg')
