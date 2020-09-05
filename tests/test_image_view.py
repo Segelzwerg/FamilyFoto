ff14256ea42d8cc32ffe65b3d7478b8979a4a01e
Segelzwerg <marcel.haas@hhu.de>
2020-09-05 13:32:16 +0200

    ADD: test for unauthorized request

--- tests/test_image_view.py
+++ tests/test_image_view.py
@@ -1,4 +1,7 @@
+from flask_api import status
+
 from family_foto.models import db
+from family_foto.models.photo import Photo
 from tests.BasePhotoTestCase import BasePhotoTestCase
 from tests.base_login_test_case import BaseLoginTestCase
 
@@ -14,10 +17,15 @@ class ImageViewTestCase(BaseLoginTestCase, BasePhotoTestCase):
         """
         db.session.add(self.photo)
         db.session.commit()
-        self.client.get('/image/example.jpg')
+        response = self.client.get('/image/example.jpg')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
 
     def test_only_show_allowed_photos(self):
         """
         Tests if the user can only see photos he/she/it has permission to view.
         """
-        pass
+        other_photo = Photo(filename='example.jpg', url='/photos/example.jpg', user=99)
+        db.session.add(other_photo)
+        db.session.commit()
+        response = self.client.get('/image/example.jpg')
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
