1b7366df61332e932aed639e333a7e619bbfc732
Segelzwerg <marcel.haas@hhu.de>
2020-09-06 12:13:58 +0200

    ADD: test for resized images route

---
+++ tests/test_resized.py
@@ -0,0 +1,17 @@
+from flask_api import status
+
+from tests.base_login_test_case import BaseLoginTestCase
+from tests.base_photo_test_case import BasePhotoTestCase
+
+
+class ResizedTestCase(BasePhotoTestCase, BaseLoginTestCase):
+    """
+    Tests the route for resizing images.
+    """
+
+    def test_route(self):
+        """
+        Tests the route is working.
+        """
+        response = self.client.get(f'/resized-images/{self.photo.filename}')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
