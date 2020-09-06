75787aecfae161602b533ed3af14abb5203d0e60
Segelzwerg <marcel.haas@hhu.de>
2020-09-06 11:24:23 +0200

    REMOVE: resize as it is not properly working

--- family_foto/resize.py
+++
@@ -1,9 +0,0 @@
-import flask_resize
-
-from family_foto.config import Config
-
-config = flask_resize.configuration.Config(
-    url=Config.RESIZE_URL,
-    root=Config.RESIZE_ROOT
-)
-resize = flask_resize.make_resizer(config)
