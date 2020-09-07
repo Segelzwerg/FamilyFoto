c51c573dd2694357a040133a35fa0be6f1ac8f29
Segelzwerg <marcel.haas@hhu.de>
2020-09-07 15:11:26 +0200

    REFACTOR: move test not testing route to own test case

--- tests/routes/test_gallery.py
+++ tests/routes/test_gallery.py
@@ -3,11 +3,10 @@ from io import BytesIO
 from flask_api import status
 from flask_login import current_user
 
-from family_foto.app import add_user
 from family_foto.models import db
 from family_foto.models.photo import Photo
-from tests.base_photo_test_case import BasePhotoTestCase
 from tests.base_login_test_case import BaseLoginTestCase
+from tests.base_photo_test_case import BasePhotoTestCase
 
 PHOTOS_SAVE_PATH = './photos'
 
@@ -55,18 +54,6 @@ class GalleryTestCase(BaseLoginTestCase, BasePhotoTestCase):
         all_photos = Photo.query.all()
         self.assertIn(other_photo, all_photos)
         self.assertNotIn(other_photo, photos)
-
-    def test_show_shared(self):
-        """
-        Tests if an user can see photos that others shared with him/her/it.
-        """
-        other_user = add_user('sharer', 'sharing')
-        other_photo = Photo(filename='other-photo.jpg', user=other_user.id)
-        db.session.add(other_photo)
-        db.session.commit()
-        other_user.share_all_with(current_user)
-        photos = current_user.get_photos()
-        self.assertIn(other_photo, photos)
 
     def test_get_original_photo(self):
         """
