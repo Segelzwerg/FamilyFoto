336aa0b8c129e4cb8c121051ea3d955a507c3e11
Segelzwerg <marcel.haas@hhu.de>
2020-09-10 16:15:34 +0200

    ADD: test for path of video

--- tests/test_video.py
+++ tests/test_video.py
@@ -13,6 +13,13 @@ class VideoTestCase(VideoBaseTestCase):
         link = 'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1920_18MG.mp4'
         print(f'example video from: {link}')
 
+    def test_path(self):
+        """
+        Tests the path property.
+        """
+        expected_path = './videos/example.mp4'
+        self.assertEqual(expected_path, self.video.path)
+
     def test_thumbnail_video(self):
         """
         Tests the rendering of the thumbnail of the video.
