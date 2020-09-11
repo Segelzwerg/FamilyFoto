b65af78e268a56eb45d3d7b58d493dcb316ab12c
Segelzwerg <marcel.haas@hhu.de>
2020-09-11 15:39:40 +0200

    ADD: test for video preview

--- tests/routes/test_video_view.py
+++ tests/routes/test_video_view.py
@@ -1,13 +1,18 @@
 from flask_api import status
 
 from family_foto.models import db
+from tests.base_login_test_case import BaseLoginTestCase
 from tests.base_video_test_case import BaseVideoTestCase
 
 
-class VideoViewTestCase(BaseVideoTestCase):
+class VideoViewTestCase(BaseLoginTestCase, BaseVideoTestCase):
     """
     Tests the route of /video/<file>
     """
+
+    def setUp(self):
+        super().setUp()
+        self.video.user = self.mock_current_user.id
 
     def test_video_view(self):
         """
@@ -15,5 +20,5 @@ class VideoViewTestCase(BaseVideoTestCase):
         """
         db.session.add(self.video)
         db.session.commit()
-        response = self.client.get('/video/example.mp4')
+        response = self.client.get('/image/example.mp4')
         self.assertEqual(status.HTTP_200_OK, response.status_code)
