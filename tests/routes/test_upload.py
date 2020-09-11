8b1f6cf4287e060809b31c054cd8b6cccfc997e0
Segelzwerg <marcel.haas@hhu.de>
2020-09-11 14:23:13 +0200

    ADD: test for uploading video

--- tests/routes/test_upload.py
+++ tests/routes/test_upload.py
@@ -5,7 +5,9 @@ from shutil import rmtree
 from flask_api import status
 
 from family_foto.models.photo import Photo
+from family_foto.models.video import Video
 from tests.base_login_test_case import BaseLoginTestCase
+from tests.base_video_test_case import VIDEOS_SAVE_PATH
 
 PHOTOS_SAVE_PATH = './photos'
 
@@ -15,9 +17,18 @@ class UploadTestCase(BaseLoginTestCase):
     Test case for uploading media files.
     """
 
+    def setUp(self):
+        super().setUp()
+        if os.path.exists(PHOTOS_SAVE_PATH):
+            rmtree(PHOTOS_SAVE_PATH)
+        if os.path.exists(VIDEOS_SAVE_PATH):
+            rmtree(VIDEOS_SAVE_PATH)
+
     def tearDown(self):
         if os.path.exists(PHOTOS_SAVE_PATH):
             rmtree(PHOTOS_SAVE_PATH)
+        if os.path.exists(VIDEOS_SAVE_PATH):
+            rmtree(VIDEOS_SAVE_PATH)
         super().tearDown()
 
     def test_upload(self):
@@ -48,3 +59,18 @@ class UploadTestCase(BaseLoginTestCase):
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         if os.path.exists(PHOTOS_SAVE_PATH):
             self.assertNotIn('foto.jpg', os.listdir(PHOTOS_SAVE_PATH))
+
+    def test_upload_video(self):
+        """
+        Tests if upload of a video works.
+        """
+        with self.client:
+            with open('../data/example.mp4', 'rb') as file:
+                data = dict(file=(file, 'example.mp4'))
+                response = self.client.post('/upload',
+                                            content_type='multipart/form-data',
+                                            data=data)
+                file.close()
+            self.assertEqual(status.HTTP_200_OK, response.status_code)
+            self.assertIn('example.mp4', os.listdir(VIDEOS_SAVE_PATH))
+            self.assertIn('example.mp4', [video.filename for video in Video.query.all()])
