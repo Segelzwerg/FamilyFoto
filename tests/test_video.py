0c5b53659a863a5964f9a5fbb1aff7ce3c4ab2bc
Segelzwerg <marcel.haas@hhu.de>
2020-09-12 13:06:16 +0200

    ADD: test for directory creation

--- tests/test_video.py
+++ tests/test_video.py
@@ -1,8 +1,9 @@
 import os
+from shutil import rmtree
 
 from PIL import Image
 
-from tests.base_video_test_case import BaseVideoTestCase
+from tests.base_video_test_case import BaseVideoTestCase, RESIZED_SAVE_PATH
 
 
 class VideoTestCase(BaseVideoTestCase):
@@ -26,6 +27,18 @@ class VideoTestCase(BaseVideoTestCase):
         """
         Tests the rendering of the thumbnail of the video.
         """
+        path = self.video.thumbnail(200, 200)
+        path = f'./{path}'
+        image = Image.open(path)
+        self.assertTrue(os.path.exists(path), msg=f'{path} does not exists.')
+        self.assertEqual(200, image.width)
+
+    def test_thumbnail_dir_not_exists(self):
+        """
+        Tests the creation of thumbnail if the thumbail directory does not exists.
+        """
+        if os.path.exists(RESIZED_SAVE_PATH):
+            rmtree(RESIZED_SAVE_PATH)
         path = self.video.thumbnail(200, 200)
         path = f'./{path}'
         image = Image.open(path)
