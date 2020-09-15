84d594bcaa91fed1ba80fd68da3828e57adeabe4
Segelzwerg <marcel.haas@hhu.de>
2020-09-15 17:16:54 +0200

    ADD: tests for token expiration

--- tests/test_auth_token.py
+++ tests/test_auth_token.py
@@ -34,3 +34,20 @@ class TestAuthTokenTestCase(BaseTestCase):
         token = AuthToken.create_token(100)
         expected_token = base64.b64encode(os_random_mock()).decode('utf-8')
         self.assertEqual(expected_token, token.token)
+
+    def test_token_check(self):
+        """
+        Test that a token is not expired.
+        """
+        token = AuthToken.create_token(300)
+        self.assertTrue(token.check(), msg=f'{token} should not have expired yet.')
+
+    @mock.patch('family_foto.models.auth_token.datetime')
+    def test_token_expired(self, mock_datetime):
+        """
+        Test token is actually expired.
+        """
+        mock_datetime.utcnow = mock.Mock(return_value=datetime(1901, 12, 21))
+        token = AuthToken.create_token(100)
+        mock_datetime.utcnow = mock.Mock(return_value=datetime(1901, 12, 21, 0, 1, 50))
+        self.assertFalse(token.check())
