8b7142804bddce51f509066af93c24e8ceb3c538
Segelzwerg <marcel.haas@hhu.de>
2020-09-16 16:38:52 +0200

    ADD: test for token verification

--- tests/api/test_auth.py
+++ tests/api/test_auth.py
@@ -2,6 +2,8 @@ from base64 import b64encode
 
 from flask_api import status
 
+from family_foto.api.auth import verify_token
+from family_foto.models.auth_token import AuthToken
 from tests.base_login_test_case import BaseLoginTestCase
 
 
@@ -16,3 +18,10 @@ class ApiAuthTestCase(BaseLoginTestCase):
                                         headers={'Authorization': f'Basic {credentials}'})
 
             self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+    def test_verify_token(self):
+        """
+        Tests the verification of a token.
+        """
+        token = AuthToken.create_token()
+        self.assertTrue(verify_token(token), msg=f'Expected {token} to be verified but was not.')
