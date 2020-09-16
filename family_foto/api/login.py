1b350659dd4bdb892eb5971331c63c2a852caaad
Segelzwerg <marcel.haas@hhu.de>
2020-09-16 16:16:42 +0200

    ADD: token getter for api

--- family_foto/api/login.py
+++ family_foto/api/login.py
@@ -1,9 +1,27 @@
+from flask import jsonify
 from flask_httpauth import HTTPBasicAuth
+from flask_login import current_user
 
+from family_foto.api import api
 from family_foto.api.errors import error_response
+from family_foto.logger import log
+from family_foto.models import db
 from family_foto.models.user import User
 
 basic_auth = HTTPBasicAuth()
+
+
+@api.route('/token', methods=['POST'])
+@basic_auth.login_required
+def get_token():
+    """
+    Retrieves a new token or the current one.
+    :return: an AuthToken in a json
+    """
+    token = basic_auth.current_user().get_token()
+    db.session.commit()
+    log.info(f'{current_user} requested AuthToken.')
+    return jsonify({'token': token})
 
 
 @basic_auth.verify_password
