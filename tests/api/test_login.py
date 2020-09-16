12535f3174f5c9b15d57aa63d674a3ed32f7ab16
Segelzwerg <marcel.haas@hhu.de>
2020-09-16 16:15:37 +0200

    ADD: test for api token getter

---
+++ tests/api/test_login.py
@@ -0,0 +1,18 @@
+from base64 import b64encode
+
+from flask_api import status
+
+from tests.base_login_test_case import BaseLoginTestCase
+
+
+class ApiLoginTestCase(BaseLoginTestCase):
+    def test_token_getter(self):
+        """
+        Tests the api route for getting a token.
+        """
+        with self.client:
+            credentials = b64encode(b'marcel:1234').decode('utf-8')
+            response = self.client.post('/api/token',
+                                        headers={'Authorization': f'Basic {credentials}'})
+
+            self.assertEqual(status.HTTP_200_OK, response.status_code)
