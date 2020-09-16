81a01bd5f4792b78dcdfe0906614d71b77da7630
Segelzwerg <marcel.haas@hhu.de>
2020-09-16 16:28:15 +0200

    REFACTOR: rename module

--- family_foto/api/__init__.py
+++ family_foto/api/__init__.py
@@ -3,5 +3,5 @@ from flask import Blueprint
 api = Blueprint('api', __name__)
 
 # avoid circular imports
-from family_foto.api import errors, login
+from family_foto.api import errors, auth
 
--- tests/api/test_auth.py
+++ tests/api/test_auth.py
@@ -5,7 +5,7 @@ from flask_api import status
 from tests.base_login_test_case import BaseLoginTestCase
 
 
-class ApiLoginTestCase(BaseLoginTestCase):
+class ApiAuthTestCase(BaseLoginTestCase):
     def test_token_getter(self):
         """
         Tests the api route for getting a token.
