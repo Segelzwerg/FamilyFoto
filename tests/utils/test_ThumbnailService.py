98189af3b45c29252e8a345a902ad33a0c61ac84
Segelzwerg <marcel.haas@hhu.de>
2020-11-15 15:56:17 +0100

    ADD: doc string

--- tests/utils/test_ThumbnailService.py
+++ tests/utils/test_ThumbnailService.py
@@ -4,7 +4,13 @@ from tests.base_photo_test_case import BasePhotoTestCase
 
 
 class TestThumbnailService(BasePhotoTestCase):
+    """
+    Tests the thumbnail service.
+    """
     def test_generate_from_photo(self):
+        """
+        Tests the thumbnail service for a photo.
+        """
         photo = Photo(filename='example.jpg', url='/photos/example.jpg')
         path = ThumbnailService.generate(photo)
         self.assertEqual('/resized-images/400_400_example.jpg', path)
