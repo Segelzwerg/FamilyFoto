ffbc6086a3ca69c4d8459f2665a14e9521ac7164
Segelzwerg <marcel.haas@hhu.de>
2020-11-15 16:19:46 +0100

    ADD: test for video thumbnail

--- tests/utils/test_thumbnail_service.py
+++ tests/utils/test_thumbnail_service.py
@@ -1,9 +1,11 @@
 from family_foto.models.photo import Photo
+from family_foto.models.video import Video
 from family_foto.utils.thumbnail_service import ThumbnailService
 from tests.base_photo_test_case import BasePhotoTestCase
+from tests.base_video_test_case import BaseVideoTestCase
 
 
-class TestThumbnailService(BasePhotoTestCase):
+class TestThumbnailService(BasePhotoTestCase, BaseVideoTestCase):
     """
     Tests the thumbnail service.
     """
@@ -14,3 +16,11 @@ class TestThumbnailService(BasePhotoTestCase):
         photo = Photo(filename='example.jpg', url='/photos/example.jpg')
         path = ThumbnailService.generate(photo)
         self.assertEqual('/resized-images/400_400_example.jpg', path)
+
+    def test_generate_from_video(self):
+        """
+        Tests the thumbnail service for a video.
+        """
+        video = Video(filename='example.mp4', url='/videos/example.mp4')
+        path = ThumbnailService.generate(video)
+        self.assertEqual('/resized-images/400_400_example.mp4', path)
