83fdf5558384acf7f9f7cd8e68165e5a592c69a5
Segelzwerg <marcel.haas@hhu.de>
2020-11-24 21:10:48 +0100

    FIX: typo

--- family_foto/utils/image.py
+++ family_foto/utils/image.py
@@ -21,7 +21,7 @@ def resize(path: str, filename: str, height: int, width: int, force: bool = Fals
     with open(path, 'rb') as file:
         if not os.path.exists(current_app.config['RESIZED_DEST']):
             os.mkdir(current_app.config['RESIZED_DEST'])
-        save_path = f'{current_app.config["RESIZED_DEST"]}/{width}_{height}_{fil ename}'
+        save_path = f'{current_app.config["RESIZED_DEST"]}/{width}_{height}_{filename}'
         if not force and os.path.exists(save_path):
             log.warning(f'Thumbnail already exists: {save_path}')
             return save_path
