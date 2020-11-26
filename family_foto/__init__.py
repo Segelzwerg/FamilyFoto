48cf84fd6fdb00a7af914cbb17b69b1777f37ebf
Segelzwerg <marcel.haas@hhu.de>
2020-11-26 23:43:47 +0100

    ADD: basic roles

--- family_foto/__init__.py
+++ family_foto/__init__.py
@@ -86,16 +86,22 @@ def create_app(test_config: dict[str, Any] = None, test_instance_path: str = Non
     from family_foto.web import photos, videos
     flask_uploads.configure_uploads(app, (photos, videos))
 
-    add_user('admin', 'admin')
+    admin_role = Role(name='admin')
+    user_role = Role(name='user')
+    db.session.add(admin_role)
+    db.session.add(user_role)
+    db.session.commit()
+    add_user('admin', 'admin', admin_role)
 
     return app
 
 
-def add_user(username: str, password: str) -> User:
+def add_user(username: str, password: str, roles: [Role]) -> User:
     """
     This registers an user.
     :param username: name of the user
     :param password: plain text password
+    :param roles: list of the roles the user has
     """
     user = User(username=username)
     user.set_password(password)
@@ -106,6 +112,7 @@ def add_user(username: str, password: str) -> User:
 
     user_settings = UserSettings(user_id=user.id)
     user.settings = user_settings
+    user.roles = roles
 
     db.session.add(user_settings)
     db.session.add(user)
