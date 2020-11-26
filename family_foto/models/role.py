4b9d96c054b503be2004a0e174e22f98c279fe64
Segelzwerg <marcel.haas@hhu.de>
2020-11-26 22:01:53 +0100

    ADD: role model

---
+++ family_foto/models/role.py
@@ -0,0 +1,15 @@
+from flask_security import RoleMixin
+
+from family_foto import db
+
+
+class Role(db.Model, RoleMixin):
+    """
+    Access role of an user.
+    """
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+    def __str__(self):
+        return self.name
