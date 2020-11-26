02de781815d1348baa482a97f9fde6f6c6f300fa
Segelzwerg <marcel.haas@hhu.de>
2020-11-26 22:59:34 +0100

    ADD: intermediate table for many to many

--- family_foto/models/__init__.py
+++ family_foto/models/__init__.py
@@ -1,3 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
+
+users_roles = db.Table('users_roles',
+                       db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
