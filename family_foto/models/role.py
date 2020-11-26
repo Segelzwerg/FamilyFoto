2685996665d7065a21c0a12a9421cdc3348822da
Segelzwerg <marcel.haas@hhu.de>
2020-11-26 22:34:45 +0100

    FIX: use correct model

--- family_foto/models/role.py
+++ family_foto/models/role.py
@@ -1,6 +1,6 @@
 from flask_security import RoleMixin
 
-from family_foto import db
+from family_foto.models import db
 
 
 class Role(db.Model, RoleMixin):
@@ -10,6 +10,7 @@ class Role(db.Model, RoleMixin):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
+    users = db.Column('User', foreign_keys='User.roles')
 
     def __str__(self):
         return self.name
