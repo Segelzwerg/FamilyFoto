b4f38a873bacf3bc884039d62a630cf379daa5b4
Segelzwerg <marcel.haas@hhu.de>
2020-11-28 18:36:33 +0100

    FIX: throw 401 for inaccessible

--- family_foto/admin/AdminModelView.py
+++ family_foto/admin/AdminModelView.py
@@ -1,16 +1,14 @@
 import flask_admin.contrib.sqla
+from flask import url_for
 from flask_login import current_user
 from werkzeug.exceptions import abort
+from werkzeug.utils import redirect
 
 
 class AdminModelView(flask_admin.contrib.sqla.ModelView):
     def is_accessible(self):
-        return (current_user.is_authenticated and current_user.has_role('admin') and
-                current_user.is_active)
+        return current_user.is_authenticated and current_user.has_role('admin')
 
-    def _handle_view(self, name, **kwargs):
-        """
-        Override builtin _handle_view in order to redirect to send 401.
-        """
-        if not self.is_accessible():
-            abort(401)
+    def inaccessible_callback(self, name, **kwargs):
+        abort(401)
+        return redirect(url_for('index.html'))
