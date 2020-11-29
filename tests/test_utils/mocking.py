05f299ac57d25b6e2fbb465b942dac798bea3ddd
Segelzwerg <marcel.haas@hhu.de>
2020-11-29 13:30:34 +0100

    ADD: doc string

--- tests/test_utils/mocking.py
+++ tests/test_utils/mocking.py
@@ -3,7 +3,13 @@ from unittest.mock import patch
 from family_foto import Role, add_user
 
 
-def mock_user(test_case, user_name, role_name):
+def mock_user(test_case, user_name, role_name) -> None:
+    """
+    Mocks a user on current_cuser
+    :param test_case: test case class, where the user should be mocked
+    :param user_name: name of the mocked user
+    :param role_name: string of the role name
+    """
     test_case.patcher = patch('flask_login.utils._get_user')
     test_case.mock_current_user = test_case.patcher.start()
     user_role = Role.query.filter_by(name=role_name).first()
