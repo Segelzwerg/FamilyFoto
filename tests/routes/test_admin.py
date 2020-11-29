5500ee0d8fe59da2bc9d3ab29e9ce86f063717b8
Segelzwerg <marcel.haas@hhu.de>
2020-11-29 11:55:41 +0100

    ADD: test for admin on admin route

--- tests/routes/test_admin.py
+++ tests/routes/test_admin.py
@@ -1,5 +1,6 @@
 from flask_api import status
 
+from family_foto import Role
 from tests.base_login_test_case import BaseLoginTestCase
 
 
@@ -7,6 +8,30 @@ class AdminTestCase(BaseLoginTestCase):
     """
     Test routes behind /admin
     """
+
+    def test_admin_route(self):
+        """
+        Test if the admin route can be reached.
+        """
+        admin_role = Role.query.filter_by(name='admin')
+        self.mock_current_user.role = [admin_role]
+        response = self.client.get('/admin/')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+    def test_admin_is_restricted(self):
+        """
+        Tests if a normal user can not view /admin
+        """
+        response = self.client.get('/admin/')
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
+
+    def test_anonymous_has_no_access(self):
+        """
+        Test that anonymous user has not access.
+        """
+        self.patcher.stop()
+        response = self.client.get('/admin/')
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
 
     def test_anonymous_has_no_access_user(self):
         """
