2774b563934e3e86a2abe37596c30ab818f0ce55
Segelzwerg <marcel.haas@hhu.de>
2020-11-29 17:20:28 +0100

    REFACTOR: metrics reset logic to base class

--- tests/routes/test_metrics.py
+++ tests/routes/test_metrics.py
@@ -6,7 +6,17 @@ from tests.base_test_case import BaseTestCase
 from tests.test_utils.mocking import mock_user
 
 
-class AdminMetricsTestCase(BaseTestCase):
+class BaseMetricsTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        # reset the underlying Prometheus registry
+        prometheus_client.REGISTRY = prometheus_client.CollectorRegistry(auto_describe=True)
+
+    def metrics(self, **kwargs):
+        return PrometheusMetrics(self.app, registry=kwargs.pop('registry', None), **kwargs)
+
+
+class AdminMetricsTestCase(BaseMetricsTestCase):
     """
     Tests the route behind /metrics with admin user.
     """
@@ -14,11 +24,6 @@ class AdminMetricsTestCase(BaseTestCase):
     def setUp(self):
         super().setUp()
         mock_user(self, 'admin', 'admin')
-        # reset the underlying Prometheus registry
-        prometheus_client.REGISTRY = prometheus_client.CollectorRegistry(auto_describe=True)
-
-    def metrics(self, **kwargs):
-        return PrometheusMetrics(self.app, registry=kwargs.pop('registry', None), **kwargs)
 
     def tearDown(self):
         self.patcher.stop()
@@ -27,3 +32,37 @@ class AdminMetricsTestCase(BaseTestCase):
     def test_metrics_route(self):
         response = self.client.get('/metrics')
         self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+
+class UserMetricsTestCase(BaseMetricsTestCase):
+    """
+    Tests the route behind /metrics with user.
+    """
+
+    def setUp(self):
+        super().setUp()
+        mock_user(self, 'user', 'user')
+
+    def tearDown(self):
+        self.patcher.stop()
+        super().tearDown()
+
+    def metrics(self, **kwargs):
+        return PrometheusMetrics(self.app, registry=kwargs.pop('registry', None), **kwargs)
+
+    def test_metrics_route(self):
+        response = self.client.get('/metrics')
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
+
+
+class AnonymousMetricsTestCase(BaseMetricsTestCase):
+    """
+    Tests the route behind /metrics with anonymous.
+    """
+
+    def setUp(self):
+        super().setUp()
+
+    def test_metrics_route(self):
+        response = self.client.get('/metrics')
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
