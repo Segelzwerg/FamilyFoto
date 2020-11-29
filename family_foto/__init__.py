3f235ef413fcc0a4a8a457f0e0716b43c5f2093e
Segelzwerg <marcel.haas@hhu.de>
2020-11-29 16:31:16 +0100

    UPDATE: metrics should only be visible to admin

--- family_foto/__init__.py
+++ family_foto/__init__.py
@@ -10,6 +10,7 @@ from prometheus_flask_exporter import PrometheusMetrics
 
 from family_foto.admin.admin_index_view import AdminHomeView
 from family_foto.admin.admin_model_view import AdminModelView
+from family_foto.admin.decorator import admin_user
 from family_foto.const import UPLOADED_PHOTOS_DEST_RELATIVE, UPLOADED_VIDEOS_DEST_RELATIVE, \
     RESIZED_DEST_RELATIVE, RESIZED_DEST
 from family_foto.logger import log
@@ -84,7 +85,7 @@ def create_app(test_config: dict[str, Any] = None, test_instance_path: str = Non
     db.app = app
     db.create_all()
 
-    metrics = PrometheusMetrics(app)
+    metrics = PrometheusMetrics(app, metrics_decorator=admin_user)
     metrics.info('app_info', 'Application info', version='0.1.2')
     metrics.register_default(
         metrics.counter(
