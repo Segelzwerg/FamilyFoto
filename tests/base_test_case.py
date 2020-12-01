eee09d51c091ec6c29b009235997d5ee558b80a9
Segelzwerg <marcel.haas@hhu.de>
2020-12-01 15:53:51 +0100

    REFACTOR: test to own class
    This reduces multiple calls of tests.

--- tests/base_test_case.py
+++ tests/base_test_case.py
@@ -55,32 +55,3 @@ class BaseTestCase(TestCase):
 
         self.assertIsNotNone(user.settings)
         self.assertEqual(user, settings.user)
-
-    # pylint: disable=too-few-public-methods, no-member
-    def test_db_migrate(self):
-        """
-        Tests the db migration.
-        """
-        meta_data = db.MetaData(bind=db.engine)
-        table: db.Table = db.Table('test', meta_data, db.Column('id', db.Integer, primary_key=True))
-        table.create()
-        table.insert().values([1]).execute()
-
-        class TestTable:
-            """
-            Empty table class.
-            """
-
-        mapper(TestTable, table)
-        instance_path = os.path.join(os.path.dirname(__file__), 'instance')
-        _ = family_foto.create_app({
-            'TESTING': True,
-            'WTF_CSRF_ENABLED': False,
-            'SQLALCHEMY_TRACK_MODIFICATIONS': True,
-            # Since we want our unit tests to run quickly
-            # we turn this down - the hashing is still done
-            # but the time-consuming part is left out.
-            'HASH_ROUNDS': 1
-        }, instance_path)
-        element = db.session.query(TestTable).all()[0]
-        self.assertEqual(1, element.id)
