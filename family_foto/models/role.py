edf811859ae4c59efcc69581ad4486c3f3c3cee8
Segelzwerg <marcel.haas@hhu.de>
2020-12-01 02:44:35 +0100

    ADD: role level

--- family_foto/models/role.py
+++ family_foto/models/role.py
@@ -9,6 +9,7 @@ class Role(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
+    level = db.Column(db.Integer(), nullable=False)
 
     def __str__(self) -> str:
         return str(self.name)
