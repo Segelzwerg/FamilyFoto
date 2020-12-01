f3d3b74da66dedd46ba84f6eadf352016bc85eba
Segelzwerg <marcel.haas@hhu.de>
2020-12-01 03:12:57 +0100

    ADD: test for unauthorized access

--- tests/routes/test_protected_gallery.py
+++ tests/routes/test_protected_gallery.py
@@ -7,3 +7,11 @@ class ProtectedGalleryTestCase(BaseLoginTestCase):
     def test_route(self):
         response = self.client.get('/public')
         self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+    def test_unauthorized_access(self):
+        """
+        Tests if a anonymous user will be redirected.
+        """
+        self.patcher.stop()
+        response = self.client.get('/public')
+        self.assertEqual(status.HTTP_302_FOUND, response.status_code)
