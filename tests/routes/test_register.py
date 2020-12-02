0a96b8c60c3b7fbd9075a42a2de3942a1d3dfd34
Segelzwerg <marcel.haas@hhu.de>
2020-12-02 15:13:23 +0100

    ADD: tests for registration

---
+++ tests/routes/test_register.py
@@ -0,0 +1,31 @@
+from flask_api import status
+
+from family_foto import User
+from tests.base_test_case import BaseTestCase
+
+
+class RegisterTestCase(BaseTestCase):
+    """
+    Tests the route behind /register.
+    """
+
+    def test_register_is_available(self):
+        """
+        Checks if the register page can be reached.
+        """
+        with self.client:
+            response = self.client.get('/register')
+            self.assertTrue(status.is_success(response.status_code))
+
+    def test_register_a_guest_user(self):
+        """
+        Checks if it is possible to register a new user.
+        """
+        with self.client:
+            response = self.client.post('register',
+                                        data={'username': 'Lea',
+                                              'password': '1234',
+                                              'password_control': '1234'})
+            user = User.query.filter_by(username='Lea').first()
+            self.assertEqual(status.HTTP_200_OK, response.status_code)
+            self.assertIsNotNone(user)
