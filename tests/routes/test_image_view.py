2e25e2c9241949f939e612099a60e45070d598a4
Segelzwerg <marcel.haas@hhu.de>
2020-12-04 22:00:22 +0100

    FIX: revoking test

--- tests/routes/test_image_view.py
+++ tests/routes/test_image_view.py
@@ -76,18 +76,22 @@ class ImageViewTestCase(BaseLoginTestCase, BasePhotoTestCase):
         self.photo.protected = False
         db.session.add(self.photo)
         db.session.commit()
-        response = self.client.get(f'/image/example.jpg')
+        response = self.client.get('/image/example.jpg')
         html_content = html.fromstring(response.data.decode('utf-8'))
         inputs = html_content.xpath('//input')
         public_share = list(filter(lambda x: x.attrib['id'] == 'public', inputs))[0]
         self.assertEqual('n', public_share.attrib['value'])
 
     def test_reset_public_status(self):
+        """
+        Tests the public access can be revoked.
+        """
         db.session.add(self.photo)
         db.session.commit()
         _ = self.client.post(f'/image/{self.photo.filename}', data=dict(public='n'))
-        response = self.client.get(f'/image/example.jpg')
+        response = self.client.get('/image/example.jpg')
         html_content = html.fromstring(response.data.decode('utf-8'))
         inputs = html_content.xpath('//input')
         public_share = list(filter(lambda x: x.attrib['id'] == 'public', inputs))[0]
+        self.assertEqual(False, self.photo.protected)
         self.assertEqual('n', public_share.attrib['value'])
