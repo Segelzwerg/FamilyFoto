46256aaaaca84f68f86d600653d9eb2fa0bb6f12
Segelzwerg <marcel.haas@hhu.de>
2020-12-05 11:38:23 +0100

    UPDATE: add a parameter to select different example files

--- tests/test_utils/tasks.py
+++ tests/test_utils/tasks.py
@@ -1,13 +1,14 @@
 import os
 
 
-def upload_test_file(client, filename='test.jpg'):
+def upload_test_file(client, filename='test.jpg', original_filename='example.jpg'):
     """
     Uploads a file from data dir to the client.
     :param client: where the file should be uploaded to
-    :param filename: name of the test file
+    :param filename: name used during upload of the test file
+    :param original_filename: name of file on the path
     """
-    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data/example.jpg')
+    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data/%s' % original_filename)
     file = open(path, 'rb')
     data = dict(file=(file, filename))
     client.post('/upload',
