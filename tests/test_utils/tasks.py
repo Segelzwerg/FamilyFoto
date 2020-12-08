de682312ebcc5b46f040494e0bc3171fedad2819
Marcel <marcel.haas@hhu.de>
2020-12-08 12:51:29 +0100

    UPDATE: use clear notation for path joining
    Co-authored-by: iTitus <iTitus@users.noreply.github.com>

--- tests/test_utils/tasks.py
+++ tests/test_utils/tasks.py
@@ -8,7 +8,7 @@ def upload_test_file(client, filename='test.jpg', original_filename='example.jpg
     :param filename: name used during upload of the test file
     :param original_filename: name of file on the path
     """
-    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data/%s' % original_filename)
+    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', original_filename)
     file = open(path, 'rb')
     data = dict(file=(file, filename))
     client.post('/upload',
