463c2da12fe9b14c8e4710659de5f206c993b225
Segelzwerg <marcel.haas@hhu.de>
2020-12-10 00:33:40 +0100

    FIX: actually remove url

--- family_foto/models/file.py
+++ family_foto/models/file.py
@@ -19,7 +19,6 @@ class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(64))
     file_type = db.Column(db.String(64))
-    # url = db.Column(db.String(128), unique=True)
     upload_date = db.Column(db.DateTime, default=datetime.utcnow)
     user = db.Column(db.Integer, ForeignKey('user.id'))
     share_with_id = db.Column(db.Integer, ForeignKey('user.id'))
