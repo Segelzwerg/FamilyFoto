e6a1cdb8183d50c81723c0f7e17157c5597962b5
Segelzwerg <marcel.haas@hhu.de>
2020-12-11 21:49:21 +0100

    UPDATE: send approvals and approve logic

--- family_foto/admin/admin_approval_view.py
+++ family_foto/admin/admin_approval_view.py
@@ -1,7 +1,23 @@
+from flask import request
 from flask_admin import BaseView, expose
+
+from family_foto.forms.approval_form import ApprovalForm
+from family_foto.models import db
+from family_foto.models.approval import Approval
+from family_foto.models.user import User
 
 
 class AdminApprovalView(BaseView):
-    @expose('/')
+    @expose('/', methods=['GET', 'POST'])
     def index(self):
-        return self.render('admin/approval.html')
+        form = ApprovalForm()
+        if request.method == 'POST':
+            for approval_id in form.users.data:
+                approval = Approval.query.get(approval_id)
+                user = User.query.get(approval.user)
+                user.approve()
+                Approval.query.filter_by(id=approval_id).delete()
+                db.session.add(user)
+            db.session.commit()
+        form.users.choices = [(approval.id, approval.user) for approval in Approval.query.all()]
+        return self.render('admin/approval.html', approval_form=form)
