d6b812b373bf43467af6cfdc892b119449fddb92
Segelzwerg <marcel.haas@hhu.de>
2020-12-15 00:19:21 +0100

    ADD: docstring

--- family_foto/admin/admin_access.py
+++ family_foto/admin/admin_access.py
@@ -6,12 +6,25 @@ from werkzeug.exceptions import abort
 
 
 class AdminAccess(BaseView):
+    """
+    Gate keeper for admin only,
+    """
+
     @abstractmethod
     def index(self):
+        """
+        Raises error because this is abstract.
+        """
         raise NotImplementedError("This should be implemented by each view class separately.")
 
     def is_accessible(self):
+        """
+        Checks if user has admin role.
+        """
         return current_user.is_authenticated and current_user.has_role('admin')
 
     def inaccessible_callback(self, name, **kwargs):
+        """
+        Aborts if not admin.
+        """
         abort(401)
