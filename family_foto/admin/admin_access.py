d90164f4d5d3e486d48213de74b499b98a478617
Segelzwerg <marcel.haas@hhu.de>
2020-12-15 00:53:26 +0100

    REMOVE: abstract index as it is handle in super class cherry-picked: 8369d985

--- family_foto/admin/admin_access.py
+++ family_foto/admin/admin_access.py
@@ -1,17 +1,21 @@
-from abc import abstractmethod
-
 from flask_admin import BaseView
 from flask_login import current_user
 from werkzeug.exceptions import abort
 
 
 class AdminAccess(BaseView):
-    @abstractmethod
-    def index(self):
-        raise NotImplementedError("This should be implemented by each view class separately.")
+    """
+    Gate keeper for admin only.
+    """
 
     def is_accessible(self):
+        """
+        Checks if user has admin role.
+        """
         return current_user.is_authenticated and current_user.has_role('admin')
 
     def inaccessible_callback(self, name, **kwargs):
+        """
+        Aborts if not admin.
+        """
         abort(401)
