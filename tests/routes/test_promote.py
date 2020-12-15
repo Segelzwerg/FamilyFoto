d3da9bd52b48c8e87c484f23f6309164dfd9b552
Segelzwerg <marcel.haas@hhu.de>
2020-12-15 01:08:00 +0100

    ADD: test for promotion route

--- tests/routes/test_promote.py
+++ tests/routes/test_promote.py
@@ -1,5 +1,7 @@
 from flask_api import status
 
+from family_foto.models.role import Role
+from family_foto.utils.add_user import add_user
 from tests.base_admin_test_case import BaseAdminTestCase
 from tests.base_login_test_case import BaseLoginTestCase
 
@@ -15,6 +17,17 @@ class PromoteTestCase(BaseAdminTestCase):
         """
         response = self.client.get('/admin/promote/')
         self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+    def test_promotion(self):
+        """
+        Tests if the promotion does work.
+        """
+        guest_role = Role.query.filter_by(level=2).first()
+        user = add_user('dummy', 'no', [guest_role])
+        data = {user.id: 'admin'}
+        response = self.client.post('/admin/promote', data=data, follow_redirects=True)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertTrue(user.has_at_least_role(1))
 
 
 class PromoteUserTestCase(BaseLoginTestCase):
