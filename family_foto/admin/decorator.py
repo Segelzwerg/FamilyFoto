cf8903c5a6776f3e2b75c28a50f2ee2a61fc969e
Segelzwerg <marcel.haas@hhu.de>
2020-12-18 13:08:16 +0100

    ADD: decorator for admin auth

--- family_foto/admin/decorator.py
+++ family_foto/admin/decorator.py
@@ -1,5 +1,8 @@
+from flask import request
 from flask_login import current_user
 from werkzeug.exceptions import abort
+
+from family_foto.models.user import User
 
 
 def admin_user(func):
@@ -14,12 +17,40 @@ def admin_user(func):
     # pylint: disable=inconsistent-return-statements
     def wrapper(*arg, **kwargs):
         """
-        Wraps the function with a check for adim privileges.
+        Wraps the function with a check for admin privileges.
         :param arg: arguments of the original function
         :param kwargs: key word arguments of the original function
         :return: 401 or function value
         """
         if current_user.is_authenticated and current_user.has_role('admin'):
+            result = func(*arg, **kwargs)
+            return result
+        abort(401)
+
+    return wrapper
+
+
+def admin_auth(func):
+    """
+    Decorator method for admin authentication.
+    :param func: function which should only be executable with admin privileges.
+    :return: the return value of the function
+    """
+
+    # If some tries to access admin area it's more like it is an intruder than user,
+    # so it's ok too simple abort.
+    # pylint: disable=inconsistent-return-statements
+    def wrapper(*arg, **kwargs):
+        """
+        Wraps the function with a check for admin privileges.
+        :param arg: arguments of the original function
+        :param kwargs: key word arguments of the original function
+        :return: 401 or function value
+        """
+        username = request.form.get('username')
+        password = request.form.get('password')
+        admin: User = User.query.filter_by(username=username).first()
+        if username == 'admin' and admin.check_password(password):
             result = func(*arg, **kwargs)
             return result
         abort(401)
