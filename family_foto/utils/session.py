c6d9de299c5aadb6b08c991bdaf241a42c29d777
Segelzwerg <marcel.haas@hhu.de>
2020-12-31 19:41:27 +0100

    ADD: doc string

--- family_foto/utils/session.py
+++ family_foto/utils/session.py
@@ -3,6 +3,11 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 
 
 def create_session(app):
+    """
+    Creates db session for multi threading with the app context.
+    :param app: the app needing a session
+    :returns: an override package Session and a session
+    """
     engine = create_engine(app.config['DATABASE_URL_TEMPLATE'])
     session_factory = sessionmaker(bind=engine)
     # this is intentional to shadow the outer scope package
