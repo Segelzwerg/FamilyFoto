474cca40f1d4e524a6bfdb89a282bdecc0cd9588
Segelzwerg <marcel.haas@hhu.de>
2020-12-31 01:19:03 +0100

    UPDATE: send all errors

--- family_foto/api/success.py
+++ family_foto/api/success.py
@@ -4,13 +4,13 @@ from werkzeug.http import HTTP_STATUS_CODES
 from family_foto.errors import FamilyFotoServerError
 
 
-def success_response(error: FamilyFotoServerError):
+def success_response(errors: [FamilyFotoServerError]):
     """
     Returns a success response.
     """
     payload: dict = {'success': HTTP_STATUS_CODES.get(200)}
-    if error:
-        payload.update({'error': error.message})
+    if len(errors) > 0:
+        payload.update({'error': errors})
     response = jsonify(payload)
     response.status_code = 200
     return response
