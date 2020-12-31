aca1013db60272d8d5a05402801cfdc65e654313
Segelzwerg <marcel.haas@hhu.de>
2020-12-31 01:10:42 +0100

    UPDATE: add partial error to response

--- family_foto/api/success.py
+++ family_foto/api/success.py
@@ -1,12 +1,16 @@
 from flask import jsonify
 from werkzeug.http import HTTP_STATUS_CODES
 
+from family_foto.errors import FamilyFotoServerError
 
-def success_response():
+
+def success_response(error: FamilyFotoServerError):
     """
     Returns a success response.
     """
-    payload = {'success': HTTP_STATUS_CODES.get(200)}
+    payload: dict = {'success': HTTP_STATUS_CODES.get(200)}
+    if error:
+        payload.update({'error': error.message})
     response = jsonify(payload)
     response.status_code = 200
     return response
