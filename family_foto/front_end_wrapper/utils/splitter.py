d2a4051baecd8cb2fe3ebdc1e165daba2b1aead2
Segelzwerg <marcel.haas@hhu.de>
2021-01-02 15:59:56 +0100

    ADD: splitter

---
+++ family_foto/front_end_wrapper/utils/splitter.py
@@ -0,0 +1,24 @@
+from typing import List, Dict
+
+from family_foto.front_end_wrapper.month import Month
+from family_foto.front_end_wrapper.year import Year
+from family_foto.models.file import File
+
+
+class Splitter:
+    """
+    Splits files into years and months.
+    """
+
+    def __init__(self, files: List[File]):
+        self._files = files
+
+    def split(self) -> Dict[int, Year]:
+        splits = dict()
+        for file in self._files:
+            if file.year not in splits.keys():
+                month = Month([file], file.month, file.year)
+                year = Year([month], file.year)
+                splits.update({file.year: year})
+
+        return splits
