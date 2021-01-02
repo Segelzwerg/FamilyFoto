2cb003aab7ee860c4f12ffd717976799e496caad
Segelzwerg <marcel.haas@hhu.de>
2021-01-02 15:59:25 +0100

    ADD: thumbnail getter ADD: equals

--- family_foto/front_end_wrapper/month.py
+++ family_foto/front_end_wrapper/month.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from family_foto import File
+from family_foto.services.thumbnail_service import generate
 
 
 class Month:
@@ -11,4 +12,16 @@ class Month:
     def __init__(self, files: List[File], month: int, year: int):
         self.month = month
         self.year = year
-        self.files = files
+        self._files = files
+
+    def __eq__(self, other):
+        if not isinstance(other, Month) or self.month != other.month or self.year != other.year:
+            return False
+        return self._files == other._files
+
+    def get_thumbnails(self, width: int, height: int) -> List[str]:
+        """
+        Returns the path to all thumbnails of the this month.
+        :return: list of paths
+        """
+        return [generate(file, width, height) for file in self._files]
--- family_foto/front_end_wrapper/year.py
+++ family_foto/front_end_wrapper/year.py
@@ -11,3 +11,8 @@ class Year:
     def __init__(self, months: List[Month], year: int):
         self.months = months
         self.year = year
+
+    def __eq__(self, other):
+        if not isinstance(other, Year) or self.year != other.year:
+            return False
+        return self.months == other.months
