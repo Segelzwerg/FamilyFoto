17bf2636983f2b138428e7b05b8b582a28256216
Segelzwerg <marcel.haas@hhu.de>
2021-01-02 16:53:21 +0100

    ADD: tests for splitter

--- tests/test_splitter.py
+++ tests/test_splitter.py
@@ -1,16 +1,41 @@
 from family_foto.front_end_wrapper.month import Month
 from family_foto.front_end_wrapper.utils.splitter import Splitter
 from family_foto.front_end_wrapper.year import Year
-from tests.base_photo_test_case import BasePhotoTestCase
+from tests.base_media_test_case import BaseMediaTestCase
 
 
-class SplitterTestCase(BasePhotoTestCase):
+class SplitterTestCase(BaseMediaTestCase):
     """
     Tests the file splitter.
     """
 
     def test_one_photo(self):
+        """
+        Tests splitting with one photo.
+        """
         splitter = Splitter([self.photo])
-        august = Month([self.photo], 8, 2020)
-        expected_year = Year([august], 2020)
-        self.assertEqual(splitter.split(), {2020: expected_year})
+        year = 2020
+        august = Month([self.photo], 8, year)
+        expected_year = Year([august], year)
+        self.assertEqual(splitter.split(), {year: expected_year})
+
+    def test_one_video(self):
+        """
+        Tests splitting with one video.
+        """
+        splitter = Splitter([self.video])
+        year = 2015
+        august = Month([self.video], 8, year)
+        expected_year = Year([august], year)
+        self.assertEqual(splitter.split(), {year: expected_year})
+
+    def test_both(self):
+        """
+        Test with both files from above.
+        """
+        splitter = Splitter([self.video, self.photo])
+        august_fifteen = Month([self.video], 8, 2015)
+        august_twenty = Month([self.photo], 8, 2020)
+        splits = splitter.split()
+        self.assertDictEqual(splits, {2020: Year([august_twenty], 2020),
+                                      2015: Year([august_fifteen], 2015)})
