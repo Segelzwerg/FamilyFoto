5614828295d17dd5937b5362e2ea7eceea8be043
Segelzwerg <marcel.haas@hhu.de>
2021-01-05 19:27:45 +0100

    ADD: methods for getting thumbnails and file to the front end

--- family_foto/front_end_wrapper/month.py
+++ family_foto/front_end_wrapper/month.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple, Iterator
 
 from family_foto import File
 from family_foto.services.thumbnail_service import generate
@@ -18,6 +18,22 @@ class Month:
         if not isinstance(other, Month) or self.month != other.month or self.year != other.year:
             return False
         return self._files == other._files
+
+    def add_file(self, file: File):
+        """
+        Adds a file to this month.
+        :param file: to be added to this year
+        """
+        self._files.append(file)
+
+    def media(self, width: int, height: int) -> Iterator[Tuple[File, str]]:
+        """
+        Gets the thumbnail paths and the files it selves.
+        :param width: of the thumbnails in pixel
+        :param height: of the thumbnails in pixel
+        :return: a tuple of the file list and a list of paths to the thumbnails
+        """
+        return zip(self._files, self.get_thumbnails(width, height))
 
     def get_thumbnails(self, width: int, height: int) -> List[str]:
         """
