1ca8c354405439e3fbb2aa966bbf571966747cd9
Segelzwerg <marcel.haas@hhu.de>
2021-01-05 15:37:54 +0100

    ADD: test for not equal

---
+++ tests/front_end_wrappers/test_month.py
@@ -0,0 +1,17 @@
+from family_foto.front_end_wrapper.month import Month
+from family_foto.front_end_wrapper.year import Year
+from tests.base_photo_test_case import BasePhotoTestCase
+
+
+class MonthTestCase(BasePhotoTestCase):
+    """
+    Test the month wrapper.
+    """
+
+    def test_not_equal(self):
+        """
+        Tests if year is not a month.
+        """
+        month = Month([self.photo], 8, 2020)
+        year = Year([month], 2020)
+        self.assertFalse(month == year)
