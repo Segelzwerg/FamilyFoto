cf795b46f896ee3773455f30c3324f15f1ebeb7f
Segelzwerg <marcel.haas@hhu.de>
2021-01-05 19:20:37 +0100

    ADD: method for adding files to a year

--- family_foto/front_end_wrapper/year.py
+++ family_foto/front_end_wrapper/year.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from family_foto.front_end_wrapper.month import Month
+from family_foto.models.file import File
 
 
 class Year:
@@ -16,3 +17,15 @@ class Year:
         if not isinstance(other, Year) or self.year != other.year:
             return False
         return self.months == other.months
+
+    def add_file(self, file: File) -> None:
+        """
+        Adds a file to this year.
+        :param file: to be added to this year
+        """
+        month_no = file.month
+        months = [month for month in self.months if month.month == month_no]
+        if len(months) == 0:
+            self.months.append(Month([file], month_no, self.year))
+        else:
+            months[0].add_file(file)
