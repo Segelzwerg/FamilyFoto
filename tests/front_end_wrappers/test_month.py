1a3b026f7db54fdbb0196a0667b0f1ad396423ac
Segelzwerg <marcel.haas@hhu.de>
2021-01-05 15:59:13 +0100

    ADD: test for thumbnail getter

--- tests/front_end_wrappers/test_month.py
+++ tests/front_end_wrappers/test_month.py
@@ -4,6 +4,10 @@ from tests.base_photo_test_case import BasePhotoTestCase
 
 
 class MonthTestCase(BasePhotoTestCase):
+    def setUp(self):
+        super().setUp()
+        self.month = Month([self.photo], 8, 2020)
+
     """
     Test the month wrapper.
     """
@@ -12,6 +16,13 @@ class MonthTestCase(BasePhotoTestCase):
         """
         Tests if year is not a month.
         """
-        month = Month([self.photo], 8, 2020)
+        month = self.month
         year = Year([month], 2020)
         self.assertFalse(month == year)
+
+    def test_thumbnails(self):
+        """
+        Tests if the thumbnails are correctly returned.
+        """
+        expected_path = f'/photos/{self.photo.hash[:2]}/{self.photo.hash}/200_200_example.jpg'
+        self.assertEqual(self.month.get_thumbnails(200, 200), [expected_path])
