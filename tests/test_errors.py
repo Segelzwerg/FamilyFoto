e52c9cb155db42b21fa2f5adaf45dec67049e5c1
Segelzwerg <marcel.haas@hhu.de>
2021-03-27 13:35:08 +0100

    ADD: doc string

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -3,7 +3,14 @@ from tests.base_test_case import BaseTestCase
 
 
 class ErrorTestCase(BaseTestCase):
+    """
+    Test the behaviour of the app's errors.
+    """
+
     def test_registration_error(self):
+        """
+        Tests error during registration process.
+        """
         field = 'password'
         reg_err = RegistrationError(field, ['This field is required.'])
         self.assertEqual(reg_err.message, f'This field is required: {field}')
