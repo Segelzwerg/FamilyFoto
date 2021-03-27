bfa023e92c38db4aa6f1e916ed23e11589d0f477
Segelzwerg <marcel.haas@hhu.de>
2021-03-27 13:28:21 +0100

    UPDATE: make email optional during validation

--- family_foto/forms/register_form.py
+++ family_foto/forms/register_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Optional
 
 
 class RegisterForm(FlaskForm):
@@ -8,7 +8,7 @@ class RegisterForm(FlaskForm):
     Form for registering an user.
     """
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[Email()])
+    email = StringField('Email', validators=[Optional(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     password_control = PasswordField('Repeat Password', validators=[DataRequired()])
     submit = SubmitField('Register')
