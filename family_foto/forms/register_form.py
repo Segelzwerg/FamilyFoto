85db4b3a20fe9af6ef2b942391e9dc3f10f4428c
Segelzwerg <marcel.haas@hhu.de>
2021-03-27 12:34:19 +0100

    ADD: email form field

--- family_foto/forms/register_form.py
+++ family_foto/forms/register_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 
 class RegisterForm(FlaskForm):
@@ -8,6 +8,7 @@ class RegisterForm(FlaskForm):
     Form for registering an user.
     """
     username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email])
     password = PasswordField('Password', validators=[DataRequired()])
     password_control = PasswordField('Repeat Password', validators=[DataRequired()])
     submit = SubmitField('Register')
