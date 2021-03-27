196de8f1d7cb96eef8835c84071d4a2f911eb8b7
Segelzwerg <marcel.haas@hhu.de>
2021-03-27 12:38:00 +0100

    FIX: validators UPDATE: make email optional

--- family_foto/forms/register_form.py
+++ family_foto/forms/register_form.py
@@ -8,7 +8,7 @@ class RegisterForm(FlaskForm):
     Form for registering an user.
     """
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email])
+    email = StringField('Email', validators=[Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     password_control = PasswordField('Repeat Password', validators=[DataRequired()])
     submit = SubmitField('Register')
