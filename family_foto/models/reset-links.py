1e21382893de2459bdf5e6d7e390aa51a731a6c1
Segelzwerg <marcel.haas@hhu.de>
2021-03-29 13:54:18 +0200

    ADD: reset link model

---
+++ family_foto/models/reset-links.py
@@ -0,0 +1,12 @@
+from family_foto.models import db
+
+
+class ResetLink(db.Model):
+    """
+    Stores links which where sent to reset a password.
+    """
+    id = db.Column(db.Integer, primary_key=True, index=True, unique=True)
+    link_hash = db.Column(db.String(32), index=True, unique=True)
+    expiration = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True, nullable=False)
+    user = db.relationship('User', foreign_keys=[user_id], uselist=False)
