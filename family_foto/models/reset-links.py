ea43500431eba9df97507e50c3138c3093eeb5ff
Segelzwerg <marcel.haas@hhu.de>
2021-03-29 14:59:56 +0200

    UPDATE: make generation static and create complete entry object

--- family_foto/models/reset-links.py
+++ family_foto/models/reset-links.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from secrets import token_urlsafe
 
 from family_foto.models import db
@@ -13,8 +14,16 @@ class ResetLink(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True, nullable=False)
     user = db.relationship('User', foreign_keys=[user_id], uselist=False)
 
-    def generate_link(self) -> None:
+    @staticmethod
+    def generate_link(user, expires_in: int = 3600) -> 'ResetLink':
         """
         Generates a hash for the password reset url.
+        :param user: the id of the user how requested a reset
+        :param expires_in: the time until the link expires. Default 6h.
         """
-        self.link_hash = token_urlsafe(32)
+        now = datetime.utcnow()
+        link_hash = token_urlsafe(32)
+        expiration = now + timedelta(seconds=expires_in)
+        link = ResetLink(user=user, link_hash=link_hash, expiration=expiration)
+        db.session.add(link)
+        return link
